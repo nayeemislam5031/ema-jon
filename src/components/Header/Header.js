d7bc5353695dfc68df2b07e3792d34c933b3680a
nayeemislam5031 <you@example.comnayeemislam5031@gmail.com>
2022-03-09 14:45:23 +0600

    final commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import logo from '../../images/logo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArchway, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import './Header.css';
 
 const Header = () => {
     return (
         <div className='header'>
             <img src={logo} alt="" />
+
+            <div className="search-area">
+            <input type="text" placeholder='Search' />
+            <button className='search-btn'> <FontAwesomeIcon icon={faMagnifyingGlass} /> </button>
+            </div>
+            
+            
 
             <nav>
             <a href="/shop">Shop</a>
